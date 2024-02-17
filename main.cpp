f87f887f4692f89b8083487d94a86033f92df56d
Vladislav Tarasov <tarvlad@inbox.ru>
2024-02-17 12:05:23 +0700

    Init v2

--- main.cpp
+++ main.cpp
@@ -3,17 +3,40 @@
 #include <print>
 #include <GLFW/glfw3.h>
 
+
+static void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+
 int main() {
     if (!glfwInit()) {
         std::println("FATAL ERROR: could not init glfw");
         return 1;
     }
 
+    // Window init
     auto *window = glfwCreateWindow(640, 480, "Test window", nullptr, nullptr);
     if (!window) {
+        glfwTerminate();
         std::println("FATAL ERROR: could not create windw");
         return 2;
     }
 
-    while (!glfwWindowShouldClose(window));
+    // Key callback setup
+    glfwSetKeyCallback(window, keyCallback);
+
+    // Load OpenGL API
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
